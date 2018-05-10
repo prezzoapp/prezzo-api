75d6fa80fb15102960d2553edeec982be4a535ee
ishmaelthedestroyer <ishmaelthedestroyer@gmail.com>
2018-05-10 14:08:50 -0700

    added babelrc + flow to files

---
+++ routes/_/index.js
@@ -0,0 +1,18 @@
+// @flow
+/* ==========================================================================
+   Local variables, module dependencies
+   ========================================================================== */
+
+import type { $Request, $Response } from 'express';
+
+/* ==========================================================================
+   Exports - route configuration
+   ========================================================================== */
+module.exports = {
+  description: 'Default route.',
+  path: '/',
+  method: 'GET',
+  run(req: $Request, res: $Response) {
+    res.$end('Hello, World');
+  }
+};
