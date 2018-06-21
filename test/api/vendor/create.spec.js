352cad5f9e242c445b0349a1370644b83fa6855c
ishmaelthedestroyer <ishmaelthedestroyer@gmail.com>
2018-06-20 20:09:32 -0700

    added unit tests for vendor creation

--- test/api/vendor/create.spec.js
+++ test/api/vendor/create.spec.js
@@ -24,7 +24,22 @@ function getPayload() {
     website: `https://${random(10)}.com/`,
     categories,
     avatarURL: `https://${random(10)}.com/${random(10)}.jpg`,
-    hours: [],
+    hours: [
+      {
+        dayOfWeek: 1,
+        openTimeHour: 8,
+        openTimeMinutes: 0,
+        closeTimeHour: 18,
+        closeTimeMinutes: 0
+      },
+      {
+        dayOfWeek: 2,
+        openTimeHour: 10,
+        openTimeMinutes: 30,
+        closeTimeHour: 20,
+        closeTimeMinutes: 45
+      }
+    ],
     location: getLocation()
   };
 }
@@ -113,7 +128,17 @@ module.exports = [
       'vendor.location.countryShort': '{{ payload.location.countryShort }}',
       'vendor.location.postalCode': '{{ payload.location.postalCode }}',
       'vendor.location.coordinates[0]': '{{ payload.location.longitude }}',
-      'vendor.location.coordinates[1]': '{{ payload.location.latitude }}'
+      'vendor.location.coordinates[1]': '{{ payload.location.latitude }}',
+      'vendor.hours[0].dayOfWeek': '{{ payload.hours[0].dayOfWeek }}',
+      'vendor.hours[0].openTimeHour': '{{ payload.hours[0].openTimeHour }}',
+      'vendor.hours[0].openTimeMinutes': '{{ payload.hours[0].openTimeMinutes }}',
+      'vendor.hours[0].closeTimeHour': '{{ payload.hours[0].closeTimeHour }}',
+      'vendor.hours[0].closeTimeMinutes': '{{ payload.hours[0].closeTimeMinutes }}',
+      'vendor.hours[1].dayOfWeek': '{{ payload.hours[1].dayOfWeek }}',
+      'vendor.hours[1].openTimeHour': '{{ payload.hours[1].openTimeHour }}',
+      'vendor.hours[1].openTimeMinutes': '{{ payload.hours[1].openTimeMinutes }}',
+      'vendor.hours[1].closeTimeHour': '{{ payload.hours[1].closeTimeHour }}',
+      'vendor.hours[1].closeTimeMinutes': '{{ payload.hours[1].closeTimeMinutes }}'
     }
   }
 ];
